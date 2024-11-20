75bd81353103ba058e01a653a4953a7a7d737eb2
Ortwin Schneider <ortwin.schneider@googlemail.com>
2024-11-20 09:47:21 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'chatbot-app',
+  webDir: 'www'
+};
+
+export default config;
